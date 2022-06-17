db3aa0203b171a08a6c0f7ac4f4a7ebc9d775f27
mnazw <mnazmiw@student.uns.ac.id>
2022-06-17 16:59:13 +0700

    fix edit from admin, fix label jenis kelamin, fix email verif, fix password hash-ing

--- app/Views/admin/index.php
+++ app/Views/admin/index.php
@@ -2,21 +2,26 @@
 <?= $this->section("content") ?>
 
 <section class="section">
-    <main>
+    <main style="margin-top: 58px">
         <div class="container">
             <div class="main-content container-fluid">
-                <div class="section-header">
-                    <h1>Daftar User</h1>
-                </div>
-                <hr>
                 <div class="row">
                     <div class="col-lg-12 margin-tb">
-                        <?php if (session()->getFlashdata('pesan')) : ?>
-                            <div class="alert alert-success" role="alert">
-                                <?= session()->getFlashdata('pesan'); ?>
+                        <div class="card">
+                            <h5 class="card-header">User List</h2>
+                            <div class="card-body">
+                            <div class="row">
+                                <div class="col-lg-12 margin-tb">
+                                    <?php if (session()->getFlashdata('pesan')) : ?>
+                                        <div class="alert alert-success" role="alert">
+                                            <?= session()->getFlashdata('pesan'); ?>
+                                        </div>
+                                    <?php endif; ?>
+                                    <div id="viewdata"></div>
+                                </div>
                             </div>
-                        <?php endif; ?>
-                        <div id="viewdata"></div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -24,6 +29,5 @@
         <div id="viewmodal" style="display: none;"></div>
     </main>
 </section>
-
 
 <?= $this->Endsection("content") ?>
--- app/Views/user/edit.php
+++ app/Views/user/edit.php
@@ -1,72 +1,81 @@
 <?= $this->extend('templates/index'); ?>
 <?= $this->section('content'); ?>
 <div class="modal fade" id="editmodal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true"></div>
-<div class="container">
-    <div class="row">
-        <div class="col-sm-12 mt-5">
-            <div class="card">
-                <h2 class="card-header">Edit User Profile</h2>
-                <div class="card-body">
-                    <form action="<?= base_url('user/update/' . user()->id) ?>" method="post" enctype="multipart/form-data" id="form">
-                        <input type="hidden" name="_method" value="PUT">
-                        <?= csrf_field() ?>
-                        <div class="form-group">
-                            <label for="fullname">Nama Lengkap</label>
-                            <input type="text" class="form-control" name="fullname" placeholder="Nama Belakang" value="<?= user()->fullname; ?>">
-                        </div>
-                        <div class="form-group">
-                            <label for="alamat">Alamat</label>
-                            <textarea type="text" class="form-control" name="alamat" placeholder="Alamat" value="<?= user()->alamat; ?>"></textarea>
-                        </div>
-                        <div class="form-group">
-                            <label for="tempat_lahir">Tempat Lahir</label>
-                            <input type="text" class="form-control" name="tempat_lahir" placeholder="Tempat Lahir" value="<?= user()->tempat_lahir; ?>">
-                        </div>
-                        <div class="form-group">
-                            <label for="tanggal_lahir">Tanggal Lahir</label>
-                            <input type="date" class="form-control" name="tanggal_lahir" placeholder="Tanggal Lahir" value="<?= user()->tanggal_lahir; ?>">
-                        </div>
-                        <span class="my-3">Jenis Kelamin: </span>
-                        <div class="form-check form-check-inline mx-2 my-3">
-                            <input class="form-check-input" type="radio" name="jenis_kelamin" id="flexRadioDefault1" value="Laki-Laki" checked />
-                            <label class="form-check-label" for="flexRadioDefault1"> Laki-laki </label>
-                        </div>
-                        <div class="form-check form-check-inline my-3">
-                            <input class="form-check-input" type="radio" name="jenis_kelamin" id="flexRadioDefault2" value="Perempuan" />
-                            <label class="form-check-label" for="flexRadioDefault2"> Perempuan </label>
-                        </div>
-                        <div class="form-group">
-                            <label for="telepon">No Telepon</label>
-                            <input type="text" class="form-control" name="telepon" placeholder="No Telepon" value="<?= user()->telepon; ?>">
-                        </div>
-                        <div class="form-group">
-                            <label for="email"><?= lang('Auth.email') ?></label>
-                            <input type="email" class="form-control <?php if (session('errors.email')) : ?>is-invalid<?php endif ?>" name="email" aria-describedby="emailHelp" placeholder="<?= lang('Auth.email') ?>" value="<?= user()->email; ?>">
-                            <small id="emailHelp" class="form-text text-muted"><?= lang('Auth.weNeverShare') ?></small>
-                        </div>
-                        <div class="form-group">
-                            <label for="username"><?= lang('Auth.username') ?></label>
-                            <input type="text" class="form-control <?php if (session('errors.username')) : ?>is-invalid<?php endif ?>" name="username" placeholder="<?= lang('Auth.username') ?>" value="<?= user()->username ?>">
-                        </div>
 
-                        <div class="form-group">
-                            <label for="password"><?= lang('Auth.password') ?></label>
-                            <input type="password" name="password" class="form-control <?php if (session('errors.password')) : ?>is-invalid<?php endif ?>" placeholder="<?= lang('Auth.password') ?>" autocomplete="off">
+<section class="section">
+    <main style="margin-top: 58px">
+        <div class="container">
+            <div class="main-content container-fluid">
+                <div class="row">
+                    <div class="col-lg-12 margin-tb">
+                        <div class="card">
+                            <h5 class="card-header">Edit User Profile</h5>
+                            <div class="card-body">
+                            <form action="<?= base_url('user/update/' . user()->id) ?>" method="post" enctype="multipart/form-data" id="form">
+                                <input type="hidden" name="_method" value="PUT">
+                                <?= csrf_field() ?>
+                                <div class="form-group">
+                                    <label for="fullname">Nama Lengkap</label>
+                                    <input type="text" class="form-control" name="fullname" placeholder="Nama Belakang" value="<?= user()->fullname; ?>">
+                                </div>
+                                <div class="form-group">
+                                    <label for="alamat">Alamat</label>
+                                    <textarea type="text" class="form-control" name="alamat" placeholder="Alamat" value="<?= user()->alamat; ?>"></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <label for="tempat_lahir">Tempat Lahir</label>
+                                    <input type="text" class="form-control" name="tempat_lahir" placeholder="Tempat Lahir" value="<?= user()->tempat_lahir; ?>">
+                                </div>
+                                <div class="form-group">
+                                    <label for="tanggal_lahir">Tanggal Lahir</label>
+                                    <input type="date" class="form-control" name="tanggal_lahir" placeholder="Tanggal Lahir" value="<?= user()->tanggal_lahir; ?>">
+                                </div>
+                                <span class="my-3">Jenis Kelamin: </span>
+                                <div class="form-check form-check-inline mx-2 my-3">
+                                    <input class="form-check-input" type="radio" name="jenis_kelamin" id="flexRadioDefault1" value="Laki-Laki" checked />
+                                    <label class="form-check-label" for="flexRadioDefault1"> Laki-laki </label>
+                                </div>
+                                <div class="form-check form-check-inline my-3">
+                                    <input class="form-check-input" type="radio" name="jenis_kelamin" id="flexRadioDefault2" value="Perempuan" />
+                                    <label class="form-check-label" for="flexRadioDefault2"> Perempuan </label>
+                                </div>
+                                <div class="form-group">
+                                    <label for="telepon">No Telepon</label>
+                                    <input type="text" class="form-control" name="telepon" placeholder="No Telepon" value="<?= user()->telepon; ?>">
+                                </div>
+                                <div class="form-group">
+                                    <label for="email"><?= lang('Auth.email') ?></label>
+                                    <input type="email" class="form-control <?php if (session('errors.email')) : ?>is-invalid<?php endif ?>" name="email" aria-describedby="emailHelp" placeholder="<?= lang('Auth.email') ?>" value="<?= user()->email; ?>">
+                                    <small id="emailHelp" class="form-text text-muted"><?= lang('Auth.weNeverShare') ?></small>
+                                </div>
+                                <div class="form-group">
+                                    <label for="username"><?= lang('Auth.username') ?></label>
+                                    <input type="text" class="form-control <?php if (session('errors.username')) : ?>is-invalid<?php endif ?>" name="username" placeholder="<?= lang('Auth.username') ?>" value="<?= user()->username ?>">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="password"><?= lang('Auth.password') ?></label>
+                                    <input type="password" name="password" class="form-control <?php if (session('errors.password')) : ?>is-invalid<?php endif ?>" placeholder="<?= lang('Auth.password') ?>" autocomplete="off">
+                                </div>
+                                <div class="align-items-center mb-4">
+                                    <label for="user_image" class="col-form-label">Foto</label>
+                                    <input type="file" class="form-control" id="user_image" name="user_image" aria-describedby="inputGroupFileAddon04" aria-label="Upload">
+                                </div>
+                                <br>
+                                <input type="hidden" id="passlama" name="passlama" value="<?= user()->password_hash; ?>" />
+                                <input type="hidden" id="avalama" name="avalama" value="<?= user()->user_image; ?>" />
+                                <button type="submit" id="submit" class="btn btn-primary mb-4">Update Data</button>
+                            </form>
+
+                            <hr>
+
+                            </div>
                         </div>
-                        <div class="align-items-center mb-4">
-                            <label for="user_image" class="col-form-label">Foto</label>
-                            <input type="file" class="form-control" id="user_image" name="user_image" aria-describedby="inputGroupFileAddon04" aria-label="Upload">
-                        </div>
-                        <br>
-                        <input type="hidden" id="passlama" name="passlama" value="<?= user()->password_hash; ?>" />
-                        <input type="hidden" id="avalama" name="avalama" value="<?= user()->user_image; ?>" />
-                        <button type="submit" id="submit" class="btn btn-primary mb-4">Update Data</button>
-                    </form>
-                    <hr>
+                    </div>
                 </div>
             </div>
-
         </div>
-    </div>
-</div>
+    </main>
+</section>
+
 <?= $this->endSection('content'); ?>
